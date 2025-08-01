41488d68051c73b5260ba59183cf89b0fb6edd57
bobbykingsada <kingsadabobby@gmail.com>
2025-08-01 09:24:45 -0700

    Initial Commit

---
+++ CommanderChessClock/CommanderChessClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommanderChessClockApp.swift
+//  CommanderChessClock
+//
+//  Created by Bobby Kingsada on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CommanderChessClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommanderChessClockTests/CommanderChessClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  CommanderChessClockTests.swift
+//  CommanderChessClockTests
+//
+//  Created by Bobby Kingsada on 8/1/25.
+//
+
+import Testing
+@testable import CommanderChessClock
+
+struct CommanderChessClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
